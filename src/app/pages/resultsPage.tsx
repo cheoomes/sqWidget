2c2e0b9d10a83346e59ad6b485542cb2ec08ac44
cheoomes <99665809+cheoomes@users.noreply.github.com>
2025-09-15 11:34:45 +0200

    fetch estimate

--- src/app/pages/resultsPage.tsx
+++ src/app/pages/resultsPage.tsx
@@ -1,26 +1,51 @@
+import { error } from "console";
 import React, { useEffect, useState } from "react";
+import { getEstimate } from "../services/estimate";
 
-type ApiResult = {
-    message: string;
-    // add other fields your API returns
-};
+import "../media/cogwheel.css";
+
+// type ApiResult = {
+//     message: string;
+//     // add other fields your API returns
+// };
 
 function Results() {
     const [loading, setLoading] = useState(true);
-    const [result, setResult] = useState<ApiResult | null>(null);
-    const [error, setError] = useState<string | null>(null);
+    const [result, setResult] = useState<any>(null);
 
     useEffect(() => {
-        const energyData = localStorage.getItem("energyData");
-        const mapCenter = localStorage.getItem("mapCenter");
+        const fetchDate = async () => {
+            try {
+                const estimate = await getEstimate();
+                setResult(estimate);
+            } catch (err: any) {
+                setError(err.message || "Something went wrong");
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchDate();
+    }, []);
 
-        if (!energyData || !mapCenter) {
-            setError("Missing data. Please fill out the previous form.");
-            setLoading(false);
-            return;
-        }
-    });
-    return <div>resultsPage</div>;
+    console.log(result);
+    return (
+        <div>
+            {loading ? (
+                <div className="waiting">
+                    <div className="cog"></div>
+                    <p>calculating ...</p>
+                </div>
+            ) : (
+                <>
+                    <h5>kWh/m²/day</h5>
+                    jan {result.irradiance["01"]}
+                </>
+            )}
+        </div>
+    );
+}
+function setError(arg0: any) {
+    throw new Error("Function not implemented.");
 }
 
 export default Results;
---
+++ src/app/services/estimate.ts
@@ -0,0 +1,31 @@
+export async function getEstimate() {
+    const energyData = localStorage.getItem("energyData");
+    const mapCenter = localStorage.getItem("mapCenter");
+
+    if (!energyData || !mapCenter) {
+        throw new Error();
+    }
+
+    const energy = JSON.parse(energyData);
+    const location = JSON.parse(mapCenter);
+    console.log(energy.usage);
+    try {
+        const response = await fetch("http://localhost:3000/api/getEstimate", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                usage: energy.usage,
+                lat: location.lat,
+                lng: location.lng,
+            }),
+        });
+
+        console.log(energy.usage);
+        const data = await response.json();
+
+        console.log(data);
+        return data;
+    } catch (err: any) {
+        throw new Error(err.message || "Something went wrong");
+    }
+}
